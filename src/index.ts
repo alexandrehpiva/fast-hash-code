f3a83e1b48a1d06fcddcd3d36a0ef3723af84169
Alexandre Piva <alexandrehpiva@gmail.com>
2022-04-14 16:28:44 -0300

    Add options parameter to allow to change the behavior of the function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
+// Adding options parameter to allow to change the behavior of the function (should be compatible with the first version of the function)
 /**
  * Generate a hash from a string, simple and fast.
- * by anonym in: https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ * reference: https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ * @version 2.0.0
  * @param {string} str Input string
+ * @param {Object} options Options
+ * @param {boolean} options.forcePositive If true, the hash will be forcePositive.
+ * @param {boolean} options.caseSensitive Case sensitive
+ * @param {boolean} options.seed Seed for the hash
  */
-function fastHashCode(str: string): number {
-    var hash = 0;
-    var i;
-    for (i = 0; i < str.length; i++) {
-        hash = (hash << 5) - hash + str.charCodeAt(i);
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-}
+export function fastHashCode(
+  str: string,
+  options: {
+    forcePositive?: boolean
+    caseSensitive?: boolean
+    seed?: number
+  } = {}
+): number {
+  const { forcePositive = false, caseSensitive = true, seed = 0 } = options
 
-export default fastHashCode
+  if (!caseSensitive) {
+    str = str.toLowerCase()
+  }
+
+  let hash = seed
+  let i
+  for (i = 0; i < str.length; i++) {
+    hash = (hash << 5) - hash + str.charCodeAt(i)
+    hash |= 0 // Convert to 32bit integer
+  }
+
+  if (forcePositive) {
+    hash = hash & 0x7fffffff
+  }
+
+  return hash
+}
